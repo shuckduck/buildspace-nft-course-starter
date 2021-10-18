64034161d3639c2890c381fb0258688caec28486
Travis <travis@ascendantridge.com>
2021-10-18 14:13:02 -0500

    contract updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import myEpicNft from './utils/MyEpicNFT.json';
 
 // Constants
+const CONTRACT_ADDRESS = "0x5dA9b671CCB486B3A537400Bc5593D099CEe9EC1";
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
@@ -29,6 +32,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account)
+      setupEventListener()
     } else {
       console.log("No authorized account found")
     }
@@ -52,6 +56,56 @@ const App = () => {
     }
   }
 
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take some time to show up. Here's the link: https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   /*
   * This runs our function when the page loads.
   */
@@ -67,7 +121,7 @@ const App = () => {
   );
 
   const renderMintNFT = () => (
-    <button onClick={null} className="cta-button mint-button">
+    <button onClick={askContractToMintNft} className="cta-button mint-button">
       Mint NFT
     </button>
   );
